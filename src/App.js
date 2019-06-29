6ae07ec426ce0b07b2b69b2a4e9e0e8a633a47e1
RJ Salamanca <salamancaronito@gmail.com>
2019-06-29 01:06:34 -0400

    finished reformatting addInput function

--- src/App.js
+++ src/App.js
@@ -20,25 +20,27 @@ class App extends Component{
         let storeLastInput = '';
         // let newInput = this.state.input + currInput;
         storeLastInput = this.state.input == '' ?  '' : this.state.input[this.state.input.length-1];
-
-        // if( (storeLastInput.match(regex) != null) && (currInput.match(regex) != null) ){
-        //     if(storeLastInput != currInput){
-        //         let changeOperator = input.innerHTML.split('');
-        //         changeOperator.splice(input.innerHTML.length-1,1,addInput)
-        //         input.innerHTML = changeOperator.join('')
-        //     }
-        // } else {
-        //     if((lastInput == '') && (addInput.match(regex) != null)){
-        //         console.log('Start with a number!');
-        //     } else {
-        //         input.innerHTML += addInput; 
-        //     }
-        // }
-
         await this.setState({
-            input: newInput,
             lastInput: storeLastInput
-        });
+        })
+        if( (storeLastInput.match(regex) != null) && (currInput.match(regex) != null) ){
+            if(storeLastInput != currInput){
+                let changeOperator = this.state.input.split('');
+                changeOperator.splice(this.state.input.length-1,1,currInput)
+                //setSTATE
+                this.setState({
+                    input: changeOperator.join('')
+                });
+            }
+        } else {
+            if((storeLastInput == '') && (currInput.toString().match(regex) != null)){
+                console.log('Start with a number!');
+            } else {
+                this.setState({
+                    input: this.state.input += currInput
+                })
+            }
+        }
 
         console.log(this.state)
     }
@@ -53,6 +55,50 @@ class App extends Component{
         });
     }
 
+    result = () => {
+        // let getNumbers = input.innerHTML.split(regex).map(curr=>Number(curr));
+        // let getOperators = input.innerHTML.replace(/[0-9]|\./g,'').split('');
+    
+        // lastInput = input.innerHTML == '' ?  '' : input.innerHTML[input.innerHTML.length-1];
+    
+        // if(!lastInput.match(regex)){
+        //     // Look for multiplication
+        //     let multiply = getOperators.indexOf('*');
+        //     while(multiply != -1){
+        //         getNumbers.splice(multiply,2, getNumbers[multiply] * getNumbers[multiply +1]);
+        //         getOperators.splice(multiply, 1);
+        //         multiply = getOperators.indexOf('*');
+        //     }
+    
+        //     // Look for division
+        //     let divide = getOperators.indexOf('/');
+        //     while(divide != -1){
+        //         getNumbers.splice(divide,2, getNumbers[divide] / getNumbers[divide +1]);
+        //         getOperators.splice(divide, 1);
+        //         divide = getOperators.indexOf('/');
+        //     }
+    
+        //     // Look for addition & subtraction
+        //     let adddition = getOperators.indexOf('+');
+        //     while(adddition != -1){
+        //         getNumbers.splice(adddition,2, getNumbers[adddition] + getNumbers[adddition +1]);
+        //         getOperators.splice(adddition, 1)
+        //         adddition = getOperators.indexOf('+')
+        //     }
+    
+        //     let subtraction = getOperators.indexOf('-');
+        //     while(subtraction != -1){
+        //         getNumbers.splice(subtraction,2, getNumbers[subtraction] - getNumbers[subtraction +1]);
+        //         getOperators.splice(subtraction, 1);
+        //         subtraction = getOperators.indexOf('-');
+        //     }
+    
+        //     console.log(getNumbers);
+        //     resultDisplayed = true;
+        //     input.innerHTML = getNumbers;
+        // }
+    }
+
     render(){
         const { result, input } = this.state;
         return (
@@ -62,7 +108,7 @@ class App extends Component{
                     <div className="buttons">
                         <Operators operatorClick={this.addInput}/>
                         <NumPad numberClick={this.addInput} clear={this.clear}/>
-                        <div className="equal" id="result">=</div>
+                        <div className="equal" id="result" onClick={(e) => this.result}>=</div>
                     </div>
                 </div>
             </div>
