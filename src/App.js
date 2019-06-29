e016f98bb5f05562d452473cbb564abefb5f86fa
RJ Salamanca <salamancaronito@gmail.com>
2019-06-29 15:47:20 -0400

    added logic for the result button calculator should be mostly working changed function name from result to calculate

--- src/App.js
+++ src/App.js
@@ -19,12 +19,13 @@ class App extends Component{
     addInput = async (currInput) => {
         let storeLastInput = '';
         // let newInput = this.state.input + currInput;
-        storeLastInput = this.state.input == '' ?  '' : this.state.input[this.state.input.length-1];
+        storeLastInput = this.state.input === '' ?  '' : this.state.input[this.state.input.length-1];
         await this.setState({
             lastInput: storeLastInput
         })
-        if( (storeLastInput.match(regex) != null) && (currInput.match(regex) != null) ){
-            if(storeLastInput != currInput){
+
+        if( (storeLastInput.toString().match(regex) !== null) && (currInput.toString().match(regex) !== null) ){
+            if(storeLastInput !== currInput){
                 let changeOperator = this.state.input.split('');
                 changeOperator.splice(this.state.input.length-1,1,currInput)
                 //setSTATE
@@ -33,7 +34,7 @@ class App extends Component{
                 });
             }
         } else {
-            if((storeLastInput == '') && (currInput.toString().match(regex) != null)){
+            if((storeLastInput === '') && (currInput.toString().match(regex) !== null)){
                 console.log('Start with a number!');
             } else {
                 this.setState({
@@ -55,52 +56,54 @@ class App extends Component{
         });
     }
 
-    result = () => {
-        // let getNumbers = input.innerHTML.split(regex).map(curr=>Number(curr));
-        // let getOperators = input.innerHTML.replace(/[0-9]|\./g,'').split('');
+    calculate = () => {
+        let { lastInput, input } = this.state;
+        let getNumbers = input.split(regex).map(curr=>Number(curr));
+        let getOperators = input.replace(/[0-9]|\./g,'').split('');
     
-        // lastInput = input.innerHTML == '' ?  '' : input.innerHTML[input.innerHTML.length-1];
+        lastInput = input === '' ?  '' : input[input.length-1];
     
-        // if(!lastInput.match(regex)){
-        //     // Look for multiplication
-        //     let multiply = getOperators.indexOf('*');
-        //     while(multiply != -1){
-        //         getNumbers.splice(multiply,2, getNumbers[multiply] * getNumbers[multiply +1]);
-        //         getOperators.splice(multiply, 1);
-        //         multiply = getOperators.indexOf('*');
-        //     }
+        if(!lastInput.match(regex)){
+            // Look for multiplication
+            let multiply = getOperators.indexOf('*');
+            while(multiply !== -1){
+                getNumbers.splice(multiply,2, getNumbers[multiply] * getNumbers[multiply +1]);
+                getOperators.splice(multiply, 1);
+                multiply = getOperators.indexOf('*');
+            }
     
-        //     // Look for division
-        //     let divide = getOperators.indexOf('/');
-        //     while(divide != -1){
-        //         getNumbers.splice(divide,2, getNumbers[divide] / getNumbers[divide +1]);
-        //         getOperators.splice(divide, 1);
-        //         divide = getOperators.indexOf('/');
-        //     }
+            // Look for division
+            let divide = getOperators.indexOf('/');
+            while(divide !== -1){
+                getNumbers.splice(divide,2, getNumbers[divide] / getNumbers[divide +1]);
+                getOperators.splice(divide, 1);
+                divide = getOperators.indexOf('/');
+            }
     
-        //     // Look for addition & subtraction
-        //     let adddition = getOperators.indexOf('+');
-        //     while(adddition != -1){
-        //         getNumbers.splice(adddition,2, getNumbers[adddition] + getNumbers[adddition +1]);
-        //         getOperators.splice(adddition, 1)
-        //         adddition = getOperators.indexOf('+')
-        //     }
+            // Look for addition & subtraction
+            let adddition = getOperators.indexOf('+');
+            while(adddition !== -1){
+                getNumbers.splice(adddition,2, getNumbers[adddition] + getNumbers[adddition +1]);
+                getOperators.splice(adddition, 1)
+                adddition = getOperators.indexOf('+')
+            }
     
-        //     let subtraction = getOperators.indexOf('-');
-        //     while(subtraction != -1){
-        //         getNumbers.splice(subtraction,2, getNumbers[subtraction] - getNumbers[subtraction +1]);
-        //         getOperators.splice(subtraction, 1);
-        //         subtraction = getOperators.indexOf('-');
-        //     }
+            let subtraction = getOperators.indexOf('-');
+            while(subtraction !== -1){
+                getNumbers.splice(subtraction,2, getNumbers[subtraction] - getNumbers[subtraction +1]);
+                getOperators.splice(subtraction, 1);
+                subtraction = getOperators.indexOf('-');
+            }
     
-        //     console.log(getNumbers);
-        //     resultDisplayed = true;
-        //     input.innerHTML = getNumbers;
-        // }
+            console.log(getNumbers);
+            this.setState({
+                input: getNumbers
+            });
+        }
     }
 
     render(){
-        const { result, input } = this.state;
+        const { input } = this.state;
         return (
             <div className="wrapper">
                 <div className="calculator">
@@ -108,7 +111,7 @@ class App extends Component{
                     <div className="buttons">
                         <Operators operatorClick={this.addInput}/>
                         <NumPad numberClick={this.addInput} clear={this.clear}/>
-                        <div className="equal" id="result" onClick={(e) => this.result}>=</div>
+                        <div className="equal" id="result" onClick={(e) => this.calculate()}>=</div>
                     </div>
                 </div>
             </div>
